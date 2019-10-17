83d6ad6ab5feb4f7061034be5a0c8c9ed17aa825
Daisy Chepkemoi <dchepkemoi408@gmail.com>
2019-10-17 13:13:07 +0300

    rnning system

---
+++ src/Liteinn.java
@@ -0,0 +1,18 @@
+
+import java.awt.Toolkit;
+import java.awt.event.WindowEvent;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Liteinn {
+
+}  
+   
---
+++ src/litein/LITEIN.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package litein;
+import java.sql.*;
+/**
+ *
+ * @author ADMIN
+ */
+public class LITEIN {
+private static final String usern="root";
+private static final String passw="kiprotichh";
+private static final String db="jdbc:mysql://localhost:3306/login";
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+    }
+    
+}
